0bc06c3ab91dea55ed30b479a27a506d3bf5f784
Najeeb NazIr <38204153+najeebnazir@users.noreply.github.com>
2018-11-24 21:08:03 +0530

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+	<title>LogIn</title>
+</head>
+<body>
+	<div class="loginBox">
+
+	<img src="index.png"  class="user">
+	<form action="process.php" method="POST">
+		
+		<center>	<h2> Log In Here </h2> </center>
+
+				<p> USERNAME </p> 
+				<input type="text" id="user" placeholder="enter your username" required >
+				<br> <br>
+				<p> PASSWORD </p> 
+				<input type="password" id="pwd" placeholder="........." required>
+	            <input type="submit" value="Log In"> 
+	            <a href="#"> Forgot Password </a>
+		
+	</form>
+
+	</div>
+
+	
+
+</body>
+</html>
